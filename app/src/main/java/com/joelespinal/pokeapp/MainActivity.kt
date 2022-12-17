faacbd0df13627110f2a32de6162980f86b0bdc0
JoelEspinal <j.espinalcarrasco@gmail.com>
2022-12-16 23:22:34 -0400

    Add google auth

--- app/src/main/java/com/joelespinal/pokeapp/MainActivity.kt
+++ app/src/main/java/com/joelespinal/pokeapp/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.joelespinal.pokeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var googleLoginButton: Button;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        googleLoginButton = findViewById(R.id.google_button)
+        googleLoginButton.setOnClickListener {
+            val intent = Intent(this, com.joelespinal.pokeapp.LoginActivity::class.java);
+            startActivity(intent)
+        }
+    }
+
+    private fun signIn() {
+        val intent = Intent("com.joelespinal.pokeapp.LoginActivity");
+        startActivityForResult(intent, MainActivity.RC_SIGN_IN)
+    }
+
+    companion object {
+        private const val TAG = "GoogleActivity"
+        private const val RC_SIGN_IN = 9001
     }
 }
