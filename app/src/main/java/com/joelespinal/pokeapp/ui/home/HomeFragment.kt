e57d4000beebbdf9716986a14651a2e597998602
JoelEspinal <j.espinalcarrasco@gmail.com>
2022-12-25 12:02:34 -0400

    Add region filters

--- app/src/main/java/com/joelespinal/pokeapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/joelespinal/pokeapp/ui/home/HomeFragment.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.joelespinal.pokeapp.R
+import com.joelespinal.pokeapp.adapters.PokemonAdapter
 import com.joelespinal.pokeapp.databinding.FragmentHomeBinding
+import me.sargunvohra.lib.pokekotlin.model.Region
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), OnItemSelectedListener {
 
     private var _binding: FragmentHomeBinding? = null
     lateinit var homeViewModel: HomeViewModel
@@ -22,7 +28,7 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
     lateinit var categoriesSpinner: Spinner
 
-    private lateinit var regionsLiveData: LiveData<List<String>>
+    private lateinit var regionsLiveData: LiveData<List<Region>>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,7 +41,7 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        regionsLiveData = homeViewModel.regionsName
+        regionsLiveData = homeViewModel.regions
         homeViewModel.getRegionNames()
 
         return root
@@ -46,15 +52,41 @@ class HomeFragment : Fragment() {
         categoriesSpinner = view.findViewById(R.id.categories_spinner)
 
       context?.let { context ->
-          var regionsAdapter = ArrayAdapter(context, android.R.layout.simple_spinner_item, mutableListOf<String>())
-            regionsAdapter.setDropDownViewResource(
-                android.R.layout.simple_spinner_dropdown_item)
-            regionsLiveData.observe( viewLifecycleOwner) { regions ->
-                regionsAdapter.addAll(regions)
-            }
+          var regionsAdapter =
+              ArrayAdapter(context, android.R.layout.simple_spinner_item, mutableListOf<String>())
+          regionsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+          regionsLiveData.observe(viewLifecycleOwner) { regions ->
+              regionsAdapter.addAll(regions.map { it.name })
+          }
 
-            categoriesSpinner.adapter = regionsAdapter
+          categoriesSpinner.adapter = regionsAdapter
+          categoriesSpinner.onItemSelectedListener = this
+      }
+        context?.let { context ->
+            val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view_pokemons)
+            val adapter = PokemonAdapter(context, homeViewModel)
+            recyclerView.adapter = adapter
+            recyclerView.layoutManager = GridLayoutManager(activity, 2)
+
+            activity?.let { activity ->
+                homeViewModel.pokemons.observe(activity) { pokemons ->
+                    adapter.submitList(pokemons)
+                }
+            }
         }
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        if(position > 0) {
+            val index = position - 1
+            val selectedRegion = regionsLiveData.value?.get(index)
+            selectedRegion?.id?.let { homeViewModel.getPokemonsByRegion(it) }
+        }
+
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 
     override fun onDestroyView() {
