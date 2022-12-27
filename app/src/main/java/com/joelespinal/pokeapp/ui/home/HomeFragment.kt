6d9a9a1b734a5dc8e5f32db98946df57d6541d15
JoelEspinal <j.espinalcarrasco@gmail.com>
2022-12-27 14:01:58 -0400

    Add resfresh indicator

--- app/src/main/java/com/joelespinal/pokeapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/joelespinal/pokeapp/ui/home/HomeFragment.kt
@@ -1,8 +1,11 @@
 package com.joelespinal.pokeapp.ui.home
 
+import android.net.ipsec.ike.ChildSessionCallback
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
+import android.view.View.OnTouchListener
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
@@ -13,6 +16,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout.OnChildScrollUpCallback
 import com.joelespinal.pokeapp.R
 import com.joelespinal.pokeapp.adapters.PokemonAdapter
 import com.joelespinal.pokeapp.databinding.FragmentHomeBinding
@@ -20,8 +25,9 @@ import me.sargunvohra.lib.pokekotlin.model.Region
 
 class HomeFragment : Fragment(), OnItemSelectedListener {
 
+    private lateinit var selectedRegion: Region
     private var _binding: FragmentHomeBinding? = null
-    lateinit var homeViewModel: HomeViewModel
+    private lateinit var homeViewModel: HomeViewModel
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -29,6 +35,7 @@ class HomeFragment : Fragment(), OnItemSelectedListener {
     lateinit var categoriesSpinner: Spinner
 
     private lateinit var regionsLiveData: LiveData<List<Region>>
+    private lateinit var swipeContainer: SwipeRefreshLayout
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -50,18 +57,29 @@ class HomeFragment : Fragment(), OnItemSelectedListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         categoriesSpinner = view.findViewById(R.id.categories_spinner)
+        swipeContainer = view.findViewById(R.id.container)
 
-      context?.let { context ->
-          var regionsAdapter =
-              ArrayAdapter(context, android.R.layout.simple_spinner_item, mutableListOf<String>())
-          regionsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-          regionsLiveData.observe(viewLifecycleOwner) { regions ->
-              regionsAdapter.addAll(regions.map { it.name })
-          }
+        initAdapter()
+        initRecycleView(view)
+    }
 
-          categoriesSpinner.adapter = regionsAdapter
-          categoriesSpinner.onItemSelectedListener = this
-      }
+    private fun initAdapter() {
+        context?.let { context ->
+            var regionsAdapter =
+                ArrayAdapter(context, android.R.layout.simple_spinner_item, mutableListOf<String>())
+            regionsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            regionsLiveData.observe(viewLifecycleOwner) { regions ->
+                regionsAdapter.addAll(regions.map { it.name })
+            }
+
+            categoriesSpinner.adapter = regionsAdapter
+            categoriesSpinner.onItemSelectedListener = this
+
+
+        }
+    }
+
+    private fun initRecycleView(view: View) {
         context?.let { context ->
             val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view_pokemons)
             val adapter = PokemonAdapter(context, homeViewModel)
@@ -73,16 +91,27 @@ class HomeFragment : Fragment(), OnItemSelectedListener {
                     adapter.submitList(pokemons)
                 }
             }
+
+            var fromIndex = 0
+               swipeContainer.setOnRefreshListener () {
+                swipeContainer.isRefreshing = false
+                fromIndex += 20
+                homeViewModel.getPokemonsByRegion(selectedRegion.id, fromIndex)
+            }
+
         }
     }
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         if(position > 0) {
-            val index = position - 1
-            val selectedRegion = regionsLiveData.value?.get(index)
-            selectedRegion?.id?.let { homeViewModel.getPokemonsByRegion(it) }
-        }
+            var index = position - 1
+            if(index >= regionsLiveData.value?.size!!) {
+                index = regionsLiveData.value?.size!! - 1
+            }
 
+            selectedRegion = regionsLiveData.value?.get(index)!!
+            selectedRegion?.id?.let { homeViewModel.getPokemonsByRegion(it, 0) }
+        }
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
