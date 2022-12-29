6298c7596ab0d39ef1c83317753db8243a63a0b4
JoelEspinal <j.espinalcarrasco@gmail.com>
2022-12-29 17:09:31 -0400

    Fix pending issues

--- app/src/main/java/com/joelespinal/pokeapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/joelespinal/pokeapp/ui/home/HomeFragment.kt
@@ -1,31 +1,30 @@
 package com.joelespinal.pokeapp.ui.home
 
-import android.net.ipsec.ike.ChildSessionCallback
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
-import android.view.View.OnTouchListener
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout.OnChildScrollUpCallback
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.joelespinal.pokeapp.R
 import com.joelespinal.pokeapp.adapters.PokemonAdapter
 import com.joelespinal.pokeapp.databinding.FragmentHomeBinding
+import me.sargunvohra.lib.pokekotlin.model.Pokemon
 import me.sargunvohra.lib.pokekotlin.model.Region
 
 class HomeFragment : Fragment(), OnItemSelectedListener {
 
-    private lateinit var selectedRegion: Region
+    private var selectedRegion: Region? = null
     private var _binding: FragmentHomeBinding? = null
     private lateinit var homeViewModel: HomeViewModel
 
@@ -36,6 +35,8 @@ class HomeFragment : Fragment(), OnItemSelectedListener {
 
     private lateinit var regionsLiveData: LiveData<List<Region>>
     private lateinit var swipeContainer: SwipeRefreshLayout
+    private lateinit var selectedPokemonsCount: TextView
+    private lateinit var createTeamButton: FloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -58,9 +59,23 @@ class HomeFragment : Fragment(), OnItemSelectedListener {
         super.onViewCreated(view, savedInstanceState)
         categoriesSpinner = view.findViewById(R.id.categories_spinner)
         swipeContainer = view.findViewById(R.id.container)
+        selectedPokemonsCount = view.findViewById(R.id.selected_pokemons_count)
+        createTeamButton = view.findViewById(R.id.create_team_button)
 
         initAdapter()
         initRecycleView(view)
+        initButtons()
+    }
+
+    private fun initButtons() {
+        homeViewModel.selectedPokemoms.observe(viewLifecycleOwner) {
+            var size = homeViewModel.selectedPokemoms.value?.size
+            selectedPokemonsCount.text = "$size"
+
+            if (size != null) {
+                createTeamButton.isEnabled = (((size > 2) && (size < 7)))
+            }
+        }
     }
 
     private fun initAdapter() {
@@ -70,12 +85,14 @@ class HomeFragment : Fragment(), OnItemSelectedListener {
             regionsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             regionsLiveData.observe(viewLifecycleOwner) { regions ->
                 regionsAdapter.addAll(regions.map { it.name })
+                categoriesSpinner.isEnabled = regions.isNotEmpty()
+//                if (regions.isNotEmpty()){
+//                    categoriesSpinner.setSelection(0)
+//                }
+
+                categoriesSpinner.adapter = regionsAdapter
+                categoriesSpinner.onItemSelectedListener = this
             }
-
-            categoriesSpinner.adapter = regionsAdapter
-            categoriesSpinner.onItemSelectedListener = this
-
-
         }
     }
 
@@ -93,12 +110,14 @@ class HomeFragment : Fragment(), OnItemSelectedListener {
             }
 
             var fromIndex = 0
-               swipeContainer.setOnRefreshListener () {
-                swipeContainer.isRefreshing = false
-                fromIndex += 20
-                homeViewModel.getPokemonsByRegion(selectedRegion.id, fromIndex)
-            }
+               swipeContainer.setOnRefreshListener {
+                   selectedRegion?.let {
+                       fromIndex += 20
+                       homeViewModel.getPokemonsByRegion(it.id, fromIndex)
+                   }
 
+                   swipeContainer.isRefreshing = false
+            }
         }
     }
 
@@ -109,8 +128,11 @@ class HomeFragment : Fragment(), OnItemSelectedListener {
                 index = regionsLiveData.value?.size!! - 1
             }
 
-            selectedRegion = regionsLiveData.value?.get(index)!!
-            selectedRegion?.id?.let { homeViewModel.getPokemonsByRegion(it, 0) }
+            selectedRegion = regionsLiveData.value?.get(index)
+            selectedRegion?.let {
+                homeViewModel.selectedRegion = selectedRegion!!
+                selectedRegion?.id?.let { homeViewModel.getPokemonsByRegion(it, 0) }
+            }
         }
     }
 
