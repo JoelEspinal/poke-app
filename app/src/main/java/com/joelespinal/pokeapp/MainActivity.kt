3e5d84385bb5c05f6578f7f4be505ab1d831f7d2
JoelEspinal <j.espinalcarrasco@gmail.com>
2023-01-11 15:39:26 -0400

    Remove google login button and add bulbasaur delay icon

--- app/src/main/java/com/joelespinal/pokeapp/MainActivity.kt
+++ app/src/main/java/com/joelespinal/pokeapp/MainActivity.kt
@@ -4,26 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var googleLoginButton: Button;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        googleLoginButton = findViewById(R.id.google_button)
-        googleLoginButton.setOnClickListener {
-            val intent = Intent(this, com.joelespinal.pokeapp.LoginActivity::class.java);
-            startActivity(intent)
-        }
     }
 
-    companion object {
-        private const val TAG = "GoogleActivity"
-        private const val RC_SIGN_IN = 9001
+    override fun onResume() {
+        super.onResume()
+        runBlocking {
+            launch {
+                delay(2000L)
+                val intent = Intent(applicationContext, com.joelespinal.pokeapp.LoginActivity::class.java);
+                startActivity(intent)
+            }
+        }
     }
 }
