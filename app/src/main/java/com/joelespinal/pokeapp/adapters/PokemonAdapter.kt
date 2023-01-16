92598eccd86e390c1d5a1e002ebc8da6741ad31d
JoelEspinal <j.espinalcarrasco@gmail.com>
2023-01-16 14:08:49 -0400

    Change color resource to contextt compat

--- app/src/main/java/com/joelespinal/pokeapp/adapters/PokemonAdapter.kt
+++ app/src/main/java/com/joelespinal/pokeapp/adapters/PokemonAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -29,8 +30,8 @@ import me.sargunvohra.lib.pokekotlin.model.Pokemon
 
         class PokemonViewHolder(itemView: View, pokmeonViewModel: PokemonViewModel) :
             RecyclerView.ViewHolder(itemView) {
-            private val pokemonImageView: ImageView = itemView.findViewById(R.id.pokemon_item_image)
-            private val pokemonName: TextView = itemView.findViewById(R.id.pokemon_item_title)
+            private val pokemonImageView: ImageView = itemView.findViewById(R.id.category_item_image)
+            private val pokemonName: TextView = itemView.findViewById(R.id.category_item_name)
             private val card: CardView = itemView.findViewById(R.id.card)
             private var itemSelected = false
 
@@ -41,20 +42,35 @@ import me.sargunvohra.lib.pokekotlin.model.Pokemon
                     .into(pokemonImageView)
 
                 pokemonName.text = pokemon?.name
-                pokemonImageView.setBackgroundColor(R.color.teal_700)
+                pokemonImageView.setBackgroundColor(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.teal_700
+                    )
+                )
 
-                card.setOnLongClickListener {
+                card.setOnClickListener() {
                     itemSelected = if (itemSelected) {
                         pokemon.let {
                             homeViewModel.deselectPokemon(it!!)
-                            pokemonImageView.setBackgroundColor(R.color.teal_700)
+                            pokemonImageView.setBackgroundColor(
+                                ContextCompat.getColor(
+                                    context,
+                                    R.color.teal_700
+                                )
+                            )
                         }
                         false
                     } else {
                        if ( homeViewModel.selectedPokemoms.value?.size!! < 6) {
                            pokemon.let{
                                homeViewModel.selectPokemon(it!!)
-                               pokemonImageView.setBackgroundColor(R.color.black)
+                               pokemonImageView.setBackgroundColor(
+                                   ContextCompat.getColor(
+                                       context,
+                                       R.color.black
+                                   )
+                               )
                            }
                        }
                         true
